86126bf9beb765c1e946312c7564b443fa6db1ab
DDDongyeon <uuas5866@naver.com>
2023-05-12 15:41:19 +0900

    1 commit

---
+++ src/main/java/multicapmpus/kb3/kb3project/Kb3projectApplication.java
@@ -0,0 +1,13 @@
+package multicapmpus.kb3.kb3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kb3projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kb3projectApplication.class, args);
+	}
+
+}
